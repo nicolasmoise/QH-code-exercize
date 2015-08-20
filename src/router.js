11979594dc74499bf614a8b02b64e4c256341e47
Nicolas Moise <nicolas@umindsoft.com>
2015-08-20 12:28:03 -0400

    remove login and register

--- src/router.js
+++ src/router.js
@@ -6,8 +6,6 @@ import http from './core/http';
 import App from './components/App';
 import ContentPage from './components/ContentPage';
 import ContactPage from './components/ContactPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
 
@@ -19,10 +17,6 @@ const router = new Router(on => {
   });
 
   on('/contact', async () => <ContactPage />);
-
-  on('/login', async () => <LoginPage />);
-
-  on('/register', async () => <RegisterPage />);
 
   on('*', async (state) => {
     const content = await http.get(`/api/content?path=${state.path}`);
